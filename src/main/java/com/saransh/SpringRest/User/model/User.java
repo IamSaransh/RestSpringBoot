72b8da1fde6371d11956d08adcb1389c0763fe1a
Saransh <saranshvashistha@gmail.com>
2022-07-07 23:52:23 +0530

    java8 -> java 11

--- src/main/java/com/saransh/SpringRest/User/model/User.java
+++ src/main/java/com/saransh/SpringRest/User/model/User.java
@@ -3,6 +3,7 @@ package com.saransh.SpringRest.User.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -16,7 +17,7 @@ import static java.lang.Math.random;
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Size(min=2)
     private String name;
